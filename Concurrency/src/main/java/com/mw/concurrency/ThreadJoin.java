3217ae5e49a8ffedfb4fd64e6b491a97d5fcae22
Muyuan Wang <wangmuyuan1@hotmail.com>
2014-08-03 22:27:41 +0100

    commit;

--- Concurrency/src/main/java/com/mw/concurrency/ThreadJoin.java
+++ Concurrency/src/main/java/com/mw/concurrency/ThreadJoin.java
@@ -1,15 +1,15 @@
 package com.mw.concurrency;
 
-public class NormalThread implements Runnable {
+public class ThreadJoin implements Runnable {
 	
 	private int id;
 	
-	public NormalThread(int id) {
+	public ThreadJoin(int id) {
 		this.id = id;
 	}
 	
 	public void run() {
-		for (int i = 0; i < 1000000; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Thread " + id + " printing " + i);
 			try {
 				Thread.sleep(1000);
@@ -20,15 +20,24 @@ public class NormalThread implements Runnable {
 	}
 	
 	public static void main (String[] args) {
-		Thread t1 = new Thread(new NormalThread(1));
-		Thread t2 = new Thread(new NormalThread(2));
+		Thread t1 = new Thread(new ThreadJoin(1));
+		Thread t2 = new Thread(new ThreadJoin(2));
 		t1.start();
-		t2.start();
 		
+		// This will not change the execution of the threads.
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// t2 won't start until t1 finishes.
+		t2.start();
 	}
 }
