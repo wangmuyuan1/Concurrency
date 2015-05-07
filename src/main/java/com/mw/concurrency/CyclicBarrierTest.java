6a3ceefc88424be3d0ba2586a35e853f98d54282
mwang <wangmuyuan1@gmail.com>
2015-05-07 16:16:11 +0100

    adfad

---
+++ src/main/java/com/mw/concurrency/CyclicBarrierTest.java
@@ -0,0 +1,72 @@
+package com.mw.concurrency;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest
+{
+
+    public static void main(String[] args)
+    {
+        Runnable runnable1 = new Runnable()
+        {
+            public void run()
+            {
+                System.out.println("Runnable 1 executed.");
+            }
+        };
+
+        Runnable runnable2 = new Runnable()
+        {
+            public void run()
+            {
+                System.out.println("Runnable 2 executed.");
+            }
+        };
+
+        CyclicBarrier barrier1 = new CyclicBarrier(2, runnable1);
+        CyclicBarrier barrier2 = new CyclicBarrier(2, runnable2);
+
+        CyclicBarrierRunnable barrierRunnable1 = new CyclicBarrierRunnable(barrier1, barrier2);
+        CyclicBarrierRunnable barrierRunnable2 = new CyclicBarrierRunnable(barrier1, barrier2);
+
+        new Thread(barrierRunnable1).start();
+        new Thread(barrierRunnable2).start();
+    }
+
+    static class CyclicBarrierRunnable implements Runnable
+    {
+        private CyclicBarrier barrier1 = null;
+        private CyclicBarrier barrier2 = null;
+
+        public CyclicBarrierRunnable(CyclicBarrier barrier1, CyclicBarrier barrier2)
+        {
+            this.barrier1 = barrier1;
+            this.barrier2 = barrier2;
+        }
+
+        public void run()
+        {
+            try
+            {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " waiting at barrier 1.");
+                barrier1.await();
+
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " waiting at barrier 2.");
+                barrier2.await();
+
+                System.out.println(Thread.currentThread().getName() + " done.");
+            }
+            catch (BrokenBarrierException e)
+            {
+                e.printStackTrace();  //TODO - remove
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();  //TODO - remove
+            }
+        }
+    }
+}
