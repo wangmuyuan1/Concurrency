376f5b73c4bce28b525ed628985077aa9e49b304
mwang <wangmuyuan1@gmail.com>
2015-09-07 15:17:36 +0100

    c12

---
+++ src/main/java/com/mw/leetcode/AddTwoNumbers.java
@@ -0,0 +1,79 @@
+package com.mw.leetcode;
+
+public class AddTwoNumbers
+{
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int currentVal = 0;
+        ListNode s1 = l1;
+        ListNode s2 = l2;
+        ListNode result = null, current = null;
+        while (s1 != null || s2 != null)
+        {
+            currentVal += (s1 == null ? 0 : s1.val) + (s2 == null ? 0 : s2.val);
+            if (current == null)
+            {
+                current = new ListNode(currentVal % 10);
+            }
+            else
+            {
+                current.next = new ListNode(currentVal % 10);
+                current = current.next;
+            }
+
+            currentVal /= 10;
+
+            if (result == null)
+            {
+                result = current;
+            }
+
+            if (s1 != null)
+            {
+                s1 = s1.next;
+            }
+
+            if (s2 != null)
+            {
+                s2 = s2.next;
+            }
+        }
+
+        if (currentVal > 0)
+        {
+            current.next = new ListNode(1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        //Output: 7 -> 0 -> 8
+        ListNode s1 = new ListNode(2);
+        s1.next = new ListNode(4);
+        s1.next.next = new ListNode(3);
+
+        ListNode s2 = new ListNode(5);
+        s2.next = new ListNode(6);
+        s2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(s1, s2);
+        while (result != null)
+        {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+}
+
+class ListNode
+{
+    int val;
+    ListNode next;
+
+    ListNode(int x)
+    {
+        val = x;
+    }
+}
---
+++ src/main/java/com/mw/leetcode/TwoSumSolution.java
@@ -0,0 +1,57 @@
+package com.mw.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumSolution
+{
+    /**
+     * Space complexity 0
+     * Time complexity n2
+     */
+    public static int[] twoSumBrutal(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int[] twoSumOptimal(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (valueMap.containsKey(nums[i]))
+            {
+                result[0] = valueMap.get(nums[i]) + 1;
+                result[1] = i + 1;
+                break;
+            }
+            else
+            {
+                valueMap.put(target - nums[i], i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] numbers = {3, 2, 4};
+        int[] result = twoSumOptimal(numbers, 6);
+        System.out.println("index1 = " + result[0]);
+        System.out.println("index2 = " + result[1]);
+    }
+}
