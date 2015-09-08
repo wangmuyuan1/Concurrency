4b350240857d4bc58aad0e17334e52b129c0f12a
mwang <wangmuyuan1@gmail.com>
2015-09-08 09:34:18 +0100

    c12

---
+++ src/main/java/com/mw/leetcode/LongestSubString.java
@@ -0,0 +1,53 @@
+package com.mw.leetcode;
+
+public class LongestSubString
+{
+    public static int lengthOfLongestSubstring(String s)
+    {
+        int[] value = new int[256];
+        char[] array = s.toCharArray();
+
+        int start = 0;
+        int length = 0;
+        int max = 0;
+        for (int i = 0; i < array.length; i++)
+        {
+            if (value[array[i]] > 0)
+            {
+                if (length > max)
+                {
+                    max = length;
+                }
+                if (value[array[i]] >= start)
+                {
+                    start = value[array[i]];
+                    length = i - value[array[i]] + 1;
+                }
+                else
+                {
+                    value[array[i]] = 0;
+                    length++;
+                }
+            }
+            else
+            {
+                length++;
+            }
+            value[array[i]] = i + 1;
+        }
+
+        if (max < length)
+        {
+            max = length;
+        }
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+        //"abcabcbb" is "abc"
+        //"bbbbb" is b
+
+        System.out.println(lengthOfLongestSubstring("tmmzuxt"));
+    }
+}
