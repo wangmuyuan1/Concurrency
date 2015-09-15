74dce998b4809e6bf987bfb5990a239dbc634482
mwang <wangmuyuan1@gmail.com>
2015-09-15 11:31:26 +0100

    c12

---
+++ src/main/java/com/mw/leetcode/p11to20/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+package com.mw.leetcode.p11to20;
+
+public class ContainerWithMostWater
+{
+    public static int maxArea(int[] height)
+    {
+        if (height.length < 2)
+            return 0;
+
+        int maxArea = 0;
+        for (int i = 0, j = height.length - 1; i < j;)
+        {
+            maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] heights = {10, 20, 51, 18, 4, 23, 77};
+        System.out.println(maxArea(heights));
+    }
+}
