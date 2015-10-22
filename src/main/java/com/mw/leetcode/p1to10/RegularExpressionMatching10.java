43b28adf647752701af8f43ab5680f84d75baa3e
mwang <wangmuyuan1@gmail.com>
2015-10-22 22:02:23 +0100

    c12

--- src/main/java/com/mw/leetcode/p1to10/RegularExpressionMatching10.java
+++ src/main/java/com/mw/leetcode/p1to10/RegularExpressionMatching10.java
@@ -2,118 +2,33 @@ package com.mw.leetcode.p1to10;
 
 public class RegularExpressionMatching10
 {
-    public static boolean isMatch1(String s, String p)
-    {
-        // for any input, if p.length is 0, s must be 0
-        if (p.length() == 0)
-            return s.length() == 0;
-
-        // for last char in p, we will never let * to be the last char as we always skip .* all together.
-        if (p.length() == 1)
-        {
-            // if the length of s is zero
-            if (s.length() < 1)
-            {
-                return false;
-            }
-            else if (s.charAt(0) != p.charAt(0) && p.charAt(0) != '.') // if p is not .
-            {
-                return false;
-            }
-            else // this char matches. but s could potentially has more char.
-            {
-                return isMatch1(s.substring(1), p.substring(1));
-            }
-        }
-
-        if (p.charAt(1) != '*') // not a* or .*
-        {
-            if (s.length() <= 1)
-            {
-                return false;
-            }
-            else if ((p.charAt(0) != s.charAt(0)) && (p.charAt(0) != '.'))
-            {
-                return false;
-            }
-            else {
-                return isMatch(s.substring(1), p.substring(1));
-            }
-        }
-        else // a* or .*
-        {
-            //case 2.1: a char & '*' can stand for 0 element
-            if (isMatch(s, p.substring(2))) {
-                return true;
-            }
-
-            //case 2.2: a char & '*' can stand for 1 or more preceding element,
-            //so try every sub string
-            int i = 0;
-            while (i<s.length() && (s.charAt(i)==p.charAt(0) || p.charAt(0)=='.')){
-                if (isMatch(s.substring(i + 1), p.substring(2))) {
-                    return true;
-                }
-                i++;
-            }
-            return false;
-        }
-    }
-
     public static boolean isMatch(String s, String p)
     {
-        // base case
-        if (p.length() == 0) {
-            return s.length() == 0;
+        if (p.length() ==0) return s.length() == 0;
+
+        if (p.length() == 1 || p.charAt(1) != '*' )
+        {
+            if (s.length() < 1 || (p.charAt(0) != '.' && p.charAt(0) != s.charAt(0)))
+            {
+                return false;
+            }
+
+            return isMatch(s.substring(1), p.substring(1));
         }
 
-        // special case
-        if (p.length() == 1) {
-
-            // if the length of s is 0, return false
-            if (s.length() < 1) {
-                return false;
-            }
-
-            //if the first does not match, return false
-            else if ((p.charAt(0) != s.charAt(0)) && (p.charAt(0) != '.')) {
-                return false;
-            }
-
-            // otherwise, compare the rest of the string of s and p.
-            else {
-                return isMatch(s.substring(1), p.substring(1));
-            }
-        }
-
-        // case 1: when the second char of p is not '*'
-        if (p.charAt(1) != '*') {
-            if (s.length() < 1) {
-                return false;
-            }
-            if ((p.charAt(0) != s.charAt(0)) && (p.charAt(0) != '.')) {
-                return false;
-            } else {
-                return isMatch(s.substring(1), p.substring(1));
-            }
-        }
-
-        // case 2: when the second char of p is '*', complex case.
-        else {
-            //case 2.1: a char & '*' can stand for 0 element
-            if (isMatch(s, p.substring(2))) {
-                return true;
-            }
-
-            //case 2.2: a char & '*' can stand for 1 or more preceding element,
-            //so try every sub string
-            int i = 0;
-            while (i<s.length() && (s.charAt(i)==p.charAt(0) || p.charAt(0)=='.')){
-                if (isMatch(s.substring(i + 1), p.substring(2))) {
+        else
+        {
+            int i = -1;
+            while (i < s.length() && (i < 0 || p.charAt(0) == '.' || p.charAt(0) == s.charAt(i)))
+            {
+                if (isMatch(s.substring(i + 1), p.substring(2))) // we have to make sure 0 is going to be matched without pre any condition.
+                                                                    // Once it is matched, we return .
+                {
                     return true;
                 }
                 i++;
             }
+
             return false;
         }
     }
