495c2180862593d827b6140d78609522e7324b52
mwang <wangmuyuan1@gmail.com>
2015-11-18 12:06:55 +0000

    New changelist

--- src/main/java/com/mw/ProjectUtil.java
+++ src/main/java/com/mw/ProjectUtil.java
@@ -3,31 +3,101 @@ package com.mw;
 import com.mw.leetcode.ListNode;
 import com.mw.leetcode.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ProjectUtil
 {
     public static TreeNode generateTree(String s)
     {
-        Deque<TreeNode> queue = new ArrayDeque<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         if (s == null && s.equals(""))
             return null;
 
         String[] array = s.split(",");
-
-        for (int i = 0; i < array.length; i++)
+        if (array[0] == null || array[0].equals("") || array[0].equals("#") || array[0].equalsIgnoreCase("null"))
         {
-            if (array[i] == null || array[i].equals("") || array[i].equals("#"))
-                continue;
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(array[0]));
+        queue.add(root);
 
-            queue.add(new TreeNode(Integer.valueOf(array[i])));
+        for (int i = 1; i < array.length; i = i + 2)
+        {
+            TreeNode node = queue.poll();
+            if (node == null)
+            {
+                throw new RuntimeException("Invalid Tree!");
+            }
 
+            if (array[i] != null && !array[i].equals("") && !array[i].equals("#") && !array[i].equalsIgnoreCase("null")) // left val valid
+            {
+                TreeNode leftChild = new TreeNode(Integer.valueOf(array[i]));
+                node.left = leftChild;
+                queue.add(leftChild);
+            }
 
+            if (i + 1 < array.length && array[i + 1] != null && !array[i + 1].equals("") && !array[i + 1].equals("#") && !array[i + 1].equalsIgnoreCase("null")) // right val valid.
+            {
+                TreeNode rightChild = new TreeNode(Integer.valueOf(array[i + 1]));
+                node.right = rightChild;
+                queue.add(rightChild);
+            }
+
+            if(
+                    (array[i] == null || array[i].equals("") || array[i].equals("#") || array[i].equalsIgnoreCase("null"))
+                && (array[i + 1] == null || array[i + 1].equals("") || array[i + 1].equals("#") || array[i + 1].equalsIgnoreCase("null"))
+                    )
+            {
+                node.left = null;
+                node.right = null;
+            }
         }
 
-        return null;
+        return root;
+    }
+
+    public static void printTree(TreeNode node)
+    {
+        Stack<TreeNode> globalStack = new Stack<>();
+        globalStack.push(node);
+        int emptyLeaf = 32;
+        boolean isRowEmpty = false;
+        System.out.println("****......................................................****");
+        while(!isRowEmpty)
+        {
+            Stack<TreeNode> localStack = new Stack<>();
+            isRowEmpty = true;
+            for(int j = 0; j < emptyLeaf; j++)
+                System.out.print(' ');
+            while(!globalStack.isEmpty())
+            {
+                TreeNode temp = globalStack.pop();
+                if(temp != null)
+                {
+                    System.out.print(temp.val);
+                    localStack.push(temp.left);
+                    localStack.push(temp.right);
+                    if(temp.left != null ||temp.right != null)
+                        isRowEmpty = false;
+                }
+                else
+                {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for(int j=0; j< emptyLeaf *2-2; j++)
+                    System.out.print(' ');
+            }
+            System.out.println();
+            emptyLeaf /= 2;
+            while(!localStack.isEmpty())
+                globalStack.push( localStack.pop() );
+        }
+        System.out.println("****......................................................****");
     }
 
     public static void printList(List list)
@@ -70,8 +140,16 @@ public class ProjectUtil
         System.out.println("NULL");
     }
 
+    private static int maxDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
     public static void main(String[] args)
     {
-        print(generateList("1->2->3->4->5->NULL"));
+        printTree(generateTree("1,#,2,3"));
     }
 }
