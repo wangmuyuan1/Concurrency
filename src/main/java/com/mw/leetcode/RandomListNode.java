9157f9b7ee7b2f117c274484a72e399ee172f46f
mwang <wangmuyuan1@gmail.com>
2015-11-26 16:58:25 +0000

    New changelist

---
+++ src/main/java/com/mw/leetcode/RandomListNode.java
@@ -0,0 +1,13 @@
+package com.mw.leetcode;
+
+public class RandomListNode
+{
+    public int label;
+    public RandomListNode next;
+    public RandomListNode random;
+
+    public RandomListNode(int x)
+    {
+        this.label = x;
+    }
+}
