752cf0473859aa8a1ff86fec6ce20ab0454df2b0
Muyuan Wang <muyuan.wang@dimensiondata.com>
2017-12-05 14:40:30 +0000

    abc

--- src/main/java/com/mw/leetcode/p121to130/BestTimetoBuyandSellStockIII.java
+++ src/main/java/com/mw/leetcode/p121to130/BestTimetoBuyandSellStockIII.java
@@ -2,41 +2,27 @@ package com.mw.leetcode.p121to130;
 
 public class BestTimetoBuyandSellStockIII
 {
-    public static int maxProfit(int[] prices)
+    public int maxProfit(int[] prices)
     {
-        if (prices.length == 0)
-            return 0;
-        int[] fromLeft = new int[prices.length];
-        int[] toRight = new int[prices.length];
-        int maxProfit = 0;
-        int max = prices[prices.length - 1];
-        int min = prices[0];
+        int sell1 = 0;
+        int sell2 = 0;
+        int buy1 = Integer.MIN_VALUE;
+        int buy2 = Integer.MIN_VALUE;
+
         for (int i = 0; i < prices.length; i++)
         {
-            min = Math.min(prices[i], min);
-            maxProfit = Math.max(prices[i] - min, maxProfit);
-            fromLeft[i] = maxProfit;
+            buy1 = Math.max(buy1, -prices[i]); // what is the lowest point if I only buy 1 time at the point of i.
+            sell1 = Math.max(sell1, buy1 + prices[i]); // what is the max profit if I only sell 1 time at the point of i.
+            buy2 = Math.max(buy2, sell1 - prices[i]); // what is the lowest point if I only buy 2 times at the point of i.
+            sell2 = Math.max(sell2, buy2 + prices[i]); // what is the max profit if I only sell 2 times at the point of i.
         }
-
-        maxProfit = 0;
-        for (int i = prices.length - 1; i >= 0; i--)
-        {
-            max = Math.max(prices[i], max);
-            maxProfit = Math.max(max - prices[i], maxProfit);
-            toRight[i] = maxProfit;
-        }
-
-        maxProfit = 0;
-        for (int i = 0; i < prices.length - 1; i++)
-        {
-            maxProfit = Math.max(fromLeft[i] + toRight[i], maxProfit);
-        }
-
-        return maxProfit;
+        return sell2;
     }
 
     public static void main(String[] args)
     {
-
+        BestTimetoBuyandSellStockIII app = new BestTimetoBuyandSellStockIII();
+        int[] array = {1,2,4,2,5,7,2,4,9,0};
+        System.out.println(app.maxProfit(array));
     }
 }
